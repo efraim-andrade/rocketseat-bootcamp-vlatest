4e2ee122456ecf55c691b54e6e50862df12bd955
Efraim Andrade <efraim.dev@gmail.com>
2020-04-19 22:03:20 -0300

    feat: 🎸 nivel_2
    added the image upload

--- nivel_2/starting-the-app-backend/aula/src/routes/users.routes.ts
+++ nivel_2/starting-the-app-backend/aula/src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -19,5 +25,27 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  },
+);
 
 export default usersRouter;
--- nivel_2/starting-the-app-backend/aula/src/server.ts
+++ nivel_2/starting-the-app-backend/aula/src/server.ts
@@ -4,9 +4,12 @@ import routes from './routes';
 
 import './database';
 
+import uploadConfig from './config/upload';
+
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
